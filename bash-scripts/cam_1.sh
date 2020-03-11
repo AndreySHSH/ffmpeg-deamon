26d839090ce342cd667466c35440fdf8c65edfdb
Andrey <laptev.andrey@icloud.com>
2020-03-11 17:26:53 +0300

    fix

--- bash-scripts/cam_1.sh
+++ bash-scripts/cam_1.sh
@@ -17,18 +17,13 @@ PROJECT_PATH=`cd $BASEDIR; pwd`
 
 
 INPUT='-fflags flush_packets -rtsp_transport  tcp -i '$1
-# VIDEO='-vsync 0 -copyts -vcodec copy -movflags frag_keyframe+empty_moov -an -sn'
-# HLS='-hls_flags delete_segments+append_list -f segment -segment_list_flags live -segment_format mpegts -segment_list'
-# OUTPUT=$PROJECT_PATH'/video_cam_'$2'/index.m3u8 -segment_list_type m3u8 '$PROJECT_PATH'/video_cam_'$2'/stream%02d.ts'
+VIDEO='-vsync 0 -copyts -vcodec copy -movflags frag_keyframe+empty_moov -an -sn'
+HLS='-hls_flags delete_segments+append_list -f segment -segment_list_flags live -segment_format mpegts -segment_list'
+OUTPUT=$PROJECT_PATH'/video_cam_'$2'/index.m3u8 -segment_list_type m3u8 '$PROJECT_PATH'/video_cam_'$2'/stream%02d.ts'
 
-# if 	[ "$3" ]; then
-# 	ffmpeg $INPUT $VIDEO $HLS $OUTPUT -loglevel debug
-# else
-# 	ffmpeg $INPUT $VIDEO $HLS $OUTPUT
-# fi
+if 	[ "$3" ]; then
+	ffmpeg $INPUT $VIDEO $HLS $OUTPUT -loglevel debug
+else
+	ffmpeg $INPUT $VIDEO $HLS $OUTPUT
+fi
 
-
-while :
-do
-ffmpeg $INPUT -fflags flush_packets -max_delay 2 -flags -global_header -hls_time 2 -hls_list_size 3 -vcodec copy -y $PROJECT_PATH'/video_cam_'$2'/index.m3u8 -segment_list_type m3u8'
-done
