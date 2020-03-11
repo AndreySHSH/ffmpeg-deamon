f675a34e09b1415f1361abd562e65ea1c6b898c2
Andrey <laptev.andrey@icloud.com>
2020-03-11 17:03:35 +0300

    fix

--- bash-scripts/cam_1.sh
+++ bash-scripts/cam_1.sh
@@ -18,4 +18,8 @@ VIDEO='-vsync 0 -copyts -vcodec copy -movflags frag_keyframe+empty_moov -an -sn'
 HLS='-hls_flags delete_segments+append_list -f segment -segment_list_flags live -segment_format mpegts -segment_list'
 OUTPUT=$PROJECT_PATH'/video_cam_'$2'/index.m3u8 -segment_list_type m3u8 '$PROJECT_PATH'/video_cam_'$2'/stream%02d.ts'
 
-ffmpeg $INPUT $VIDEO $HLS $OUTPUT $3 
+if 	[ "$3" ]; then
+	ffmpeg $INPUT $VIDEO $HLS $OUTPUT -debug
+else
+	ffmpeg $INPUT $VIDEO $HLS $OUTPUT
+fi
