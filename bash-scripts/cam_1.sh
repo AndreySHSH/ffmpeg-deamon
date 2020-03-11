46ec00d70f9e0496d6dc8e014eec7898949213ec
Andrey <laptev.andrey@icloud.com>
2020-03-11 15:31:44 +0300

    create scripts for cams

---
+++ bash-scripts/cam_1.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -       
+#title           :cam_1.sh
+#description     :This script will make a stream from the RSTP protocol
+#author		 	 :Andrey SHSH
+#date            :11032020
+#version         :0.4    
+#usage		 	 :bash cam_1.sh
+#notes           :empty
+#bash_version    :1.0-release
+#==============================================================================
+
+
+BASEDIR=`dirname $0`
+PROJECT_PATH=`cd $BASEDIR; pwd`
+
+INPUT='-fflags nobuffer -rtsp_transport  tcp -i rtsp://192.168.1.64:554/live_ch02_01'
+VIDEO='-vsync 0 -copyts -vcodec copy -movflags frag_keyframe+empty_moov -an'
+HLS='-hls_flags delete_segments+append_list -f segment -segment_list_flags live -segment_time 1 -segment_list_size 1 -segment_format mpegts -segment_list'
+OUTPUT=$PROJECT_PATH'/video_cam_1/index.m3u8 -segment_list_type m3u8 '$PROJECT_PATH'/video_cam_1/stream%05d.ts'
+
+ffmpeg $INPUT $VIDEO $HLS $OUTPUT
